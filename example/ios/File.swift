02baff7be1f81414e146120926be3b0b6c8d539d
Terry_J <terry889@availasoft.com>
2022-06-13 00:51:56 +0800

    chore: initial commit

---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  IronsourceadsExample
+//
+
+import Foundation
---
+++ ios/Ironsourceads.h
@@ -0,0 +1,5 @@
+#import <React/RCTBridgeModule.h>
+
+@interface Ironsourceads : NSObject <RCTBridgeModule>
+
+@end
